a1f303ff050513c63d7c620b731e74451e9e80f5
jakubmraz <58209778+jakubmraz@users.noreply.github.com>
2020-10-07 10:19:01 +0200

    +coffee class

---
+++ src/js/coffee.ts
@@ -0,0 +1,14 @@
+export abstract class Coffee{
+    discount:number;
+
+    constructor(discount?:number){
+        if(discount == void 0){ discount = 0 }
+        this.discount = discount;
+    }
+
+    abstract Price():number;
+    
+    Strength():string{
+        return "Mild";
+    }
+}
